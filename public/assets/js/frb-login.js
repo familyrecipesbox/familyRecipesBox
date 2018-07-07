db0dea9dcbb6830f2056cd3272954353ccdf9ab3
Mary Bellapu <marybellapu@gmail.com>
2018-07-07 12:12:19 -0700

    Updated with logout

--- public/assets/js/frb-login.js
+++ public/assets/js/frb-login.js
@@ -5,11 +5,21 @@
           firebase.auth().signInWithPopup(provider)
           .then(result=>{
               const user=result.user;
-              document.write("Hello! "+user.displayName);
+              sessionStorage.setItem("uid",user.uid);
+              sessionStorage.setItem("name",user.displayName);
+              window.location = 'recipes.html';
+              //document.write("Hello! "+user.displayName);
               console.log(user);
 
           }).catch(
               console.log()
           )
+      }
 
+      function logout(){
+        firebase.auth().signOut().then(function() {
+            window.location="index.html";
+          }).catch(function(error) {
+            // An error happened.
+          });
       }
