1d3010610b9a0feb6c7cf07238196a52ad35f129
Mary Bellapu <marybellapu@gmail.com>
2018-07-10 00:50:54 -0700

    add recipe files

--- public/assets/js/frb-login.js
+++ public/assets/js/frb-login.js
@@ -1,44 +1,64 @@
 
 
-      function googleLogin(){
-          const provider=new firebase.auth.GoogleAuthProvider();
-          firebase.auth().signInWithPopup(provider)
-          .then(result=>{
-              const user=result.user;
-              sessionStorage.setItem("uid",user.uid);
-              sessionStorage.setItem("name",user.displayName);
-              sessionStorage.setItem("pp",user.photoURL);
-               // Code for handling the push
-            database.ref("users").push({
-                "id": user.uid,
-                "fullName": user.displayName,
-                "email":user.email,
-                "photoURL":user.photoURL,
-                "providerId":"google.com",
-                "dateAdded": firebase.database.ServerValue.TIMESTAMP
+function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+        .then(result => {
+            const user = result.user;
+            const id = user.uid;
+            sessionStorage.setItem("uid", user.uid);
+            sessionStorage.setItem("name", user.displayName);
+            sessionStorage.setItem("pp", user.photoURL);
+            let users = database.ref("users");
+            users.once('value', function (snapshot) {
+                if (!snapshot.hasChild(id)) {
+                    // Code for handling the push
+                    database.ref("users/" + id).set({
+                        "id": user.uid,
+                        "fullName": user.displayName,
+                        "email": user.email,
+                        "photoURL": user.photoURL,
+                        "providerId": "google.com",
+                        "dateAdded": firebase.database.ServerValue.TIMESTAMP,
+                        "dateUpdated": "",
+                        "loginTime": firebase.database.ServerValue.TIMESTAMP,
+                        "logoutTime": ""
+                    });
+                } else {
+                    database.ref("users/" + id).update({
+                        "dateUpdated": firebase.database.ServerValue.TIMESTAMP,
+                        "loginTime": firebase.database.ServerValue.TIMESTAMP,
+                        "logoutTime": ""
+                    });
+                }
+                console.log(user);
+                window.location = 'recipes.html';
             });
-              window.location = 'recipes.html';
-              console.log(user);
-
-          }).catch(function(error){
-            console.log("error"+error);          }
-              
-          )
-      }
-
-      firebase.auth().signInAnonymously().catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        
-      });
 
 
 
-      function logout(){
-        firebase.auth().signOut().then(function() {
-            window.location="index.html";
-          }).catch(function(error) {
-            // An error happened.
-          });
-      }
+        }).catch(function (error) {
+            console.log("error" + error);
+        }
+
+        )
+}
+
+
+
+
+
+function logout() {
+    firebase.auth().signOut().then(function () {
+        const id = sessionStorage.getItem("uid");
+
+
+        database.ref("users/" + id).update({
+            "logoutTime": firebase.database.ServerValue.TIMESTAMP
+        });
+
+        window.location = "index.html";
+    }).catch(function (error) {
+        // An error happened.
+    });
+}
