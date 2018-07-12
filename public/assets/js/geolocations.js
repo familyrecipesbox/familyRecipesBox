c4d54861421ed0d7f02e5efa4d64216612c40dfc
Mary Bellapu <marybellapu@gmail.com>
2018-07-12 00:33:25 -0700

    updated add recipe

--- public/assets/js/geolocations.js
+++ public/assets/js/geolocations.js
@@ -16,7 +16,7 @@ function showPosition(position) {
     var myLatLng = { lat: position.coords.latitude, lng: position.coords.longitude };
 
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 14,
+        zoom: 12,
         center: myLatLng
     });
 
--- public/assets/js/recipes.js
+++ public/assets/js/recipes.js
@@ -1,30 +1,41 @@
 
-  var quill = new Quill('#editor', {
+var quill = new Quill('#editor', {
     theme: 'snow'
-  });
+});
 
+let recipe = {};
+const id = sessionStorage.getItem("uid");
+let ingIndex = 1;
 
-$("#add-recipe").on("click", function (event) {
-    const id = sessionStorage.getItem("uid");
-    event.preventDefault();
-    let recipe = new Object();
+function addIng() {
+    let name = "ing" + ingIndex;
+    let ingsdiv = $("<div>")
+        .addClass("ings");
+    let txt = $("<input>")
+        .attr("type", "text")
+        .attr("id", name + "-name");
+    let quantOptions = $("<select>")
+        .addClass("form-control quantity")
+        .attr("id", name + "-quantity")
+        .html("<option id=''>Please select</option>");
+    ingsdiv.append(txt, quantOptions);
+    $("#ing-list").append(ingsdiv);
+    loadQuantities(name + "-quantity");
+    ingIndex++;
+}
 
-    const metadata={
-        contentType:"image/jpeg"
-    };
-    recipe.name = $("#name").val().trim();
+function uploadPic() {
     recipe.pic = document.getElementById("pic").files[0];
-    recipe.cuisine = $("#cuisines").val();
-    recipe.category = $("#categories").val();
-    var notes = $('#notes');
-    notes.val( JSON.stringify(quill.getContents()));
-    recipe.notes = $("#notes").val();
+
     let file = new File([recipe.pic], recipe.pic, { "type": "image/jpeg" });
     var fileName = document.getElementById("pic").files[0].name;
+
     console.log(document.getElementById("pic").files[0]);
     console.log(file);
-    let uploadTask = storage.ref("images/" + id + "/" + fileName).put(file,metadata);
-    let url = "";
+
+    let uploadTask = storage.ref("images/" + id + "/" + fileName).put(file);
+    recipe.url = "";
+
     uploadTask.on('state_changed', function (snapshot) {
         // Observe state change events such as progress, pause, and resume
         // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
@@ -45,49 +56,98 @@ $("#add-recipe").on("click", function (event) {
         // Handle successful uploads on complete
         // For instance, get the download URL: https://firebasestorage.googleapis.com/...
         uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-            url = downloadURL;
+            recipe.url = downloadURL;
+            $("#recipe-pic").removeAttr("data-src");
+            $("#recipe-pic").attr("src", recipe.url);
             console.log('File available at', downloadURL);
-            // Code for handling the push
-            database.ref("users/" + id + "/recipes/"+recipe.name.replace(" ","")).set({
-                "name": recipe.name,
-                "cuisine": recipe.cuisine,
-                "pic": url,
-                "category": recipe.category,
-                "notes": recipe.notes,
-                "dateAdded": firebase.database.ServerValue.TIMESTAMP,
-                "dateUpdated": ""
-            });
-            clear();
-            swal("Looks yum!", "Added recipe to your box!!", "success");
         });
-
     });
+}
 
+$("#add-recipe").on("click", function (event) {
 
+    event.preventDefault();
+    if(recipe.url==undefined){
+        recipe.url="";
+    }
+    recipe.name = $("#name").val().trim();
+    recipe.cuisine = $("#cuisines").val();
+    recipe.category = $("#categories").val();
+    var notes = $('#notes');
+    notes.val(JSON.stringify(quill.getContents()));
+    recipe.notes = $("#notes").val();
+
+    if (validate($("#name")) && validate($("#notes"))) {
+        // Code for handling the push
+        database.ref("users/" + id + "/recipes/" + recipe.name.replace(" ", "")).set({
+            "name": recipe.name,
+            "cuisine": recipe.cuisine,
+            "pic": recipe.url,
+            "category": recipe.category,
+            "notes": recipe.notes,
+            "dateAdded": firebase.database.ServerValue.TIMESTAMP,
+            "dateUpdated": ""
+        });
+       // clear();
+        swal("Looks yum!", "Added recipe to your box!!", "success");
+        window.location="recipes.html";
+    }
 });
 
+//Validate element is blank
+function validate(element) {
+    if (element.val().trim() == "") {
+        $("#" + element.attr("id") + "-error").show();
+        element.focus();
+        return false;
+    } else {
+        $("#" + element.attr("id") + "-error").hide();
+        return true;
+    }
+}
 
-function clear(){
+function clear() {
     $("#name").val("");
     $("#pic").val("");
     $("#cuisines").val("");
     $("#categories").val("");
     $("#notes").val("");
-   quill.setContents("");
+    quill.setContents("");
     $("#uploader").val(0);
+    $(".ings").remove();
+    $("#recipe-pic").removeAttr("src");
+    $("#recipe-pic").attr("data-src", "holder.js/50px75px?theme=thumb&bg=cccccc&fg=eceeef&text=Recipe pic");
+}
+
+function loadQuantities(elementId) {
+    database.ref("quantities").once("value", function (snapshot) {
+        snapshot.forEach(function (quantity) {
+            let option = $("<option>").html(quantity.val().name).val(quantity.val().id);
+            $("#" + elementId).append(option);
+        });
+    });
 }
 
 $(document).ready(function () {
+   //Hiding validation error divs
+    $(".error").hide();
+
     database.ref("cuisines").once("value", function (snapshot) {
-        snapshot.forEach(function(cuisine) {
+        snapshot.forEach(function (cuisine) {
             let option = $("<option>").html(cuisine.val().name).val(cuisine.val().id);
             $("#cuisines").append(option);
         });
     });
     database.ref("categories").once("value", function (snapshot) {
-        snapshot.forEach(function(category) {
+        snapshot.forEach(function (category) {
             let option = $("<option>").html(category.val().name).val(category.val().id);
             $("#categories").append(option);
         });
     });
+    /*database.ref("quantities").once("value", function (snapshot) {
+        snapshot.forEach(function (quantity) {
+            let option = $("<option>").html(quantity.val().name).val(quantity.val().id);
+            $("#ing1-quantity").append(option);
+        });
+    });*/
 });
